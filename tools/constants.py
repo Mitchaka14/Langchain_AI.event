ef6de16b06f55780491223c5a513e0b955b3ccbe
Mitchaka14 <105875322+Mitchaka14@users.noreply.github.com>
2023-07-21 16:39:12 -0600

    added gitignore

--- tools/constants.py
+++
@@ -1,4 +0,0 @@
-# Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
-# and rename this file to constants.py.
-APIKEY = "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"
-SERPAPI_API_KEY = "dc6c5f1d0c2d34020c44261c2ffd27b5b60c64b14c7d646ff296b68de718347c"
